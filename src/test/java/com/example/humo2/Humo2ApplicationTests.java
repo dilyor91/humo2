c371ac7f7e07ae2b6e06078b2b6a3f0b08e5f761
Dilyor Toshboltayev <dilyor_05@mail.ru>
2020-11-21 15:52:01 +0500

    first commit

---
+++ src/test/java/com/example/humo2/Humo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Humo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
