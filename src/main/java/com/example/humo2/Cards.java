2dc5e6179bcbf21f3af7b4d8ac87153308c236cf
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-11-24 10:14:41 +0500

    Prepare Dto

--- src/main/java/com/example/humo2/Cards.java
+++ src/main/java/com/example/humo2/Cards.java
@@ -1,6 +1,9 @@
 package com.example.humo2;
 
+import com.example.humo2.dto.ClientDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,52 +17,7 @@ import java.net.URLConnection;
 @RequestMapping(value = "/api")
 public class Cards {
     @PostMapping(value = "/balance")
-    public String getBalance() throws IOException {
-      /*
-        System.out.println("hghgjhj");
-        String cardNumber = "9860270101631892";
-        String responseString = "";
-        String outputString = "";
-        String wsURL = "https://192.168.35.22:6677/";
-        URL url = new URL(wsURL);
-        URLConnection connection = url.openConnection();
-        HttpURLConnection httpConn = (HttpURLConnection) connection;
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        String xmlInput = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:IIACardServices\">\n" +
-                "                   <soapenv:Header/>\n" +
-                "                   <soapenv:Body>\n" +
-                "                   <urn:getCardAccountsBalance>\n" +
-                "                   <primaryAccountNumber>" + cardNumber + "</primaryAccountNumber>\n" +
-                "                   </urn:getCardAccountsBalance>\n" +
-                "                   </soapenv:Body>\n" +
-                "                   </soapenv:Envelope>";
-        byte[] buffer = new byte[xmlInput.length()];
-        buffer = xmlInput.getBytes();
-        bout.write(buffer);
-        byte[] b = bout.toByteArray();
-        String SOAPAction = "";
-        httpConn.setRequestProperty("Content-Length", String.valueOf(b.length));
-        httpConn.setRequestProperty("Authorization", "Basic d3NvZmI6MUYyZUp4QG0=");
-        httpConn.setRequestProperty("Content-Type", "text/xml;charset=utf-8");
-        httpConn.setRequestProperty("SOAPAction", SOAPAction);
-        httpConn.setRequestMethod("POST");
-        httpConn.setDoOutput(true);
-        httpConn.setDoInput(true);
-        OutputStream out = httpConn.getOutputStream();
-        //Write the content of the request to the outputstream of the HTTP Connection.
-        out.write(b);
-        out.close();
-        //Ready with sending the request.
-
-        //Read the response.
-        InputStreamReader isr = new InputStreamReader(httpConn.getInputStream());
-        BufferedReader in = new BufferedReader(isr);
-
-        while ((responseString = in.readLine()) != null) {
-            outputString = outputString + responseString;
-        }
-        */
-
-        return  new HumoGetApi().poster();
+    public ResponseEntity<ClientDto> getBalance(@RequestBody ClientDto clientDto) throws IOException {
+        return  ResponseEntity.ok(clientDto);
     }
 }
---
+++ src/main/java/com/example/humo2/dto/ClientDto.java
@@ -0,0 +1,19 @@
+package com.example.humo2.dto;
+
+/*
+ *
+ *  @A Sabirov Jakhongir
+ *
+ */
+public class ClientDto {
+    private String client;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+}
