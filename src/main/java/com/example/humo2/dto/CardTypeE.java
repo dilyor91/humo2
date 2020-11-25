16febea9a732dac812920b2863dd37eceefbcf16
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-11-25 11:26:45 +0500

    Worked once so far.

---
+++ src/main/java/com/example/humo2/dto/CardTypeE.java
@@ -0,0 +1,12 @@
+package com.example.humo2.dto;
+
+/*
+ *
+ *  @A Sabirov Jakhongir
+ *
+ */
+public enum CardTypeE {
+    sv,
+    gl,
+    tet
+}
--- src/test/java/com/example/humo2/Humo2ApplicationTests.java
+++ src/test/java/com/example/humo2/Humo2ApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.humo2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.util.Arrays;
+
+
+@RunWith(SpringRunner.class)
 class Humo2ApplicationTests {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String uri = "http://10.10.120.123:8080/api/balance";
+    private final String request = "<PaymentCardRequest5 xmlns=\"http://bssys.com/sbns/integration\">\n" +
+            " <head>\n" +
+            "  <Head>\n" +
+            "   <branchId>01071</branchId>\n" +
+            "   <customerId>591840</customerId>\n" +
+            "  </Head>\n" +
+            " </head>\n" +
+            "</PaymentCardRequest5>";
 
     @Test
     void contextLoads() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
+        httpHeaders.set("Content-Type","application/xml");
+
+        HttpEntity<String> entity = new HttpEntity<String>(request,httpHeaders);
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST,entity,String.class);
+
+        if(response.getStatusCode().is2xxSuccessful()){
+            System.out.println(response);
+        }
+
+
     }
 
 }
